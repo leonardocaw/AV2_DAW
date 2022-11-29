f125a848a60969db2b8065b994b238b21f46072e
leonardocaw <101294364+leonardocaw@users.noreply.github.com>
2022-11-29 13:33:17 -0300

    Add files via upload

--- AV2_DAW/locadora.js
+++ AV2_DAW/locadora.js
@@ -17,3 +17,56 @@ function enviarCarro() { //(Nome, Marca, Cor, Valor) {
     xmlHttp.send();
     console.log("Enviei requisição");
 }
+function criarLinhaTabela(linha) {
+    let tabela = document.getElementById("tabela");
+    let tr = document.createElement("tr");
+    
+    //Coluna nome
+    let nome = document.createElement("td"); 
+    textnode = document.createTextNode(linha.nome);
+    tdcarro.appendChild(textnode); 
+    tr.appendChild(nome);
+  
+    //Coluna modelo
+    let modelo = document.createElement("td"); 
+    textnode = document.createTextNode(linha.modelo);
+    tdtipo.appendChild(textnode); 
+    tr.appendChild(modelo); 
+  
+    //Coluna ano
+    let ano = document.createElement("td");
+    textnode = document.createTextNode(linha.ano);
+    tdano.appendChild(textnode); 
+    tr.appendChild(ano);
+  
+    let cor = document.createElement("td");
+    textnode = document.createTextNode(linha.cor);
+    tdano.appendChild(textnode); 
+    tr.appendChild(cor);
+
+    let valor = document.createElement("td");
+    textnode = document.createTextNode(linha.valor);
+    tdano.appendChild(textnode); 
+    tr.appendChild(valor);   
+  }
+
+function carregaCarros() {
+    let xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                let obj = JSON.parse(this.responseText);
+                let x = 0;
+                
+                let valor = (obj[x].local).localeCompare("undefined");
+                if(valor!=0){
+                        for (x=0;x<obj.length;x++) {
+                        let linha = obj[x];
+                        criarLinhaTabela(linha);
+                    }
+                }
+            }
+        }
+        xmlHttp.open("GET", "http://localhost/AV2_DAW/exibirCarro.php");
+        xmlHttp.send();
+  }
+  
